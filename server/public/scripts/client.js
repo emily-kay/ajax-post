b5a068eee17cbe046b3a227b15347e3b9c2d8319
Emily Kay <emilykay917@gmail.com>
2018-04-19 11:27:06 -0500

    Added array through AJAX

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,14 @@ console.log('client.js is up');
 $(document).ready(onReady);
 
 function onReady(){
-    console.log('jquery is up');
-}
+    $.ajax({
+        type: 'GET',
+        url: '/records'
+    })//end .ajax
+    .then(function (response){
+        response.forEach((record) => {
+            $("#listOfRecords").append(`<li>"${record.title}" by ${record.artist} <br> Current cost: ${record.cost}!!</li>`);
+        });
+    });//end .then
+}//end onReady
 
--- server/server.js
+++ server/server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+//this will eventually be found in the database
+const recordCollection = require('./modules/record-collection.js');
+
+
 app.use(express.static('server/public'));
+
+
+//routes (get,post,put delete requests)
+app.get('/records', (req, res) =>{
+    res.send(recordCollection);
+});
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
